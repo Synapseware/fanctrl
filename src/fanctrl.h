b84620d83b4b26ebe281e3dccc3043860ed83dcd
Ramun McCallum <matthew@synapseware.com>
2016-09-25 13:02:16 -0700

    Working on PWM frequency & config

--- src/fanctrl.h
+++ src/fanctrl.h
@@ -6,13 +6,19 @@
 #include <avr/interrupt.h>
 #include <inttypes.h>
 #include <avr/sleep.h>
+#include <avr/power.h>
 
 
-#define F_MAX    127
+#define F_MAX		255
 
-const float      SCALE      = (float)(F_MAX/255.0);
-const uint8_t    MAX        = (uint8_t)(F_MAX * 0.90);
+const float			SCALE		= (float)(F_MAX/255.0);
+const uint8_t		MAX			= (uint8_t)(F_MAX * 0.90);
 
+#define	ADC_IN		ADC3D
+#define ADC_PIN		PB3
+
+
+#define PWM_PIN		PB4
 
 
 #endif
